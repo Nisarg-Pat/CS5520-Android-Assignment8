331e5b7e9d483c78ba68f660e404c0dbc2597202
Sespeck <jacfan17@gmail.com>
2022-07-09 14:40:02 -0400

    Unversioned files added from Chris

---
+++ app/src/main/java/edu/neu/harshit/gajjar/numadsp22_team24_a8/StickerActivity.java
@@ -0,0 +1,42 @@
+package edu.neu.harshit.gajjar.numadsp22_team24_a8;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StickerActivity extends AppCompatActivity {
+    private final int[] STICKER_IDS = new int[] {R.drawable.sticker1,
+            R.drawable.sticker2,R.drawable.sticker3,R.drawable.sticker4,
+            R.drawable.sticker5,R.drawable.sticker6,R.drawable.sticker7,
+            R.drawable.sticker8,R.drawable.sticker9,R.drawable.sticker10};
+    private StickerNotification notification;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sticker_layout);
+        this.notification = new StickerNotification(this);
+        List<Sticker> stickerList = new ArrayList<>();
+        for (int Id:
+             STICKER_IDS) {
+            // This needs to be changed to reflect the stored count in the db
+            stickerList.add(new Sticker("",Id,0));
+
+        }
+        StickerAdapter adapter = new StickerAdapter(this,stickerList);
+        RecyclerView view = findViewById(R.id.sticker_recycler_view);
+        view.setHasFixedSize(true);
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            view.setLayoutManager(new GridLayoutManager(this, 2));
+        } else{
+            view.setLayoutManager(new GridLayoutManager(this, 3));
+        }
+        view.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/edu/neu/harshit/gajjar/numadsp22_team24_a8/StickerNotification.java
@@ -0,0 +1,56 @@
+package edu.neu.harshit.gajjar.numadsp22_team24_a8;
+
+import android.app.Activity;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+public class StickerNotification {
+    private static final String CHANNEL_1_ID = "CHANNEL1";
+    private static final int NOTIFICATION_ID = 1;
+    private Activity activity;
+
+
+    public StickerNotification(Activity activity){
+        this.activity = activity;
+        registerNotificationChannel();
+    }
+
+    /**
+     * Creates a new notification with the passed in username.
+     * @param username The username to be displayed.
+     */
+    public void createNotification(String username){
+        NotificationCompat.Builder builder = new
+                NotificationCompat.Builder(activity, CHANNEL_1_ID)
+                .setSmallIcon(R.drawable.notification_message)
+                .setContentTitle(username)
+                .setContentText("New Sticker!")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(activity);
+        notificationManager.notify(NOTIFICATION_ID,builder.build());
+    }
+
+    /**
+     * Registers a notification channel. Necessary so phones running newer APIs
+     * receive the notifications.
+     */
+    private void registerNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_1_ID, "Channel 1", importance);
+            channel.setDescription("Notification channel 1");
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = activity.
+                    getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+}
